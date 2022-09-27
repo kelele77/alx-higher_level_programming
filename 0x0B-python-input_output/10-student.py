55e0b65ec8abc99146aefae749dec70618927dd5
Kelechi <kelechianthony0@gmail.com>
2022-09-27 05:43:05 -0700

    Input/Output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -27,30 +27,3 @@ class Student:
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
-
-    def reload_from_json(self, json):
-        """Replace all attributes of the Student.
-        Args:
-            json (dict): The key/value pairs to replace attributes with.
-        """
-        for k, v in json.items():
-            setattr(self, k, v)#!/usr/bin/python3
-''' a class Student that defines a student
-'''
-
-
-class Student:
-    '''module class student
-    '''
-
-    def __init__(self, first_name, last_name, age):
-        '''method __init__
-        '''
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self):
-        '''method to_json
-        '''
-        return self.__dict__
